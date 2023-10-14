237fb329d31b2abf776bea6b0236f434cc48f9e7
Bhoomika <bhoomikahm18@gmail.com>
2023-10-14 12:33:23 +0530

    CSS property for buttons

--- script.js
+++ script.js
@@ -78,29 +78,28 @@ const matrixGenerator = (cardValues, size = 4) => {
     cardValues.sort(() => Math.random() - 0.5);
     for (let i = 0; i < size * size; i++) {
         /*
-       Create Cards
-       before => front side (contains question mark)
-       after => back side (contains actual image);
-       data-card-values is a custom attribute which stores the names of the cards to match later
-     */
+            Create Cards
+            before => front side (contains question mark)
+            after => back side (contains actual image);
+            data-card-values is a custom attribute which stores the names of the cards to match later
+          */
         gameContainer.innerHTML += `
-        <div class="card-container" data-card-value="${cardValues[i].name}">
-            <div class="card-before">?</div>
-            <div class="card-after">
-            <img src="${cardValues[i].image}" class="image"/></div>
-        </div>
-    `;
+       <div class="card-container" data-card-value="${cardValues[i].name}">
+          <div class="card-before">?</div>
+          <div class="card-after">
+          <img src="${cardValues[i].image}" class="image"/></div>
+       </div>
+       `;
     }
     //Grid
     gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
-
     //Cards
     cards = document.querySelectorAll(".card-container");
     cards.forEach((card) => {
-        card.addEventListener('click', () => {
-            //If selected card is not matched yet them only run(i.e already matched card when clicked would be ignored)
-            if (!card.classList.contains("mateched")) {
-                //flip the clicked card
+        card.addEventListener("click", () => {
+            //If selected card is not matched yet then only run (i.e already matched card when clicked would be ignored)
+            if (!card.classList.contains("matched")) {
+                //flip the cliked card
                 card.classList.add("flipped");
                 //if it is the firstcard (!firstCard since firstCard is initially false)
                 if (!firstCard) {
@@ -108,34 +107,42 @@ const matrixGenerator = (cardValues, size = 4) => {
                     firstCard = card;
                     //current cards value becomes firstCardValue
                     firstCardValue = card.getAttribute("data-card-value");
-                }
-            }
-            else {
-                //increament moves since user selected second card
-                movesCounter();
-                //secondCard and value
-                let secondCardValue = card.getAttribute("data-card-value");
-                if (firstCardValue == secondCardValue) {
-                    //if both cards match add matched class so these cards would beignored next time
-                    firstCard.classList.add("matched");
-                    secondCard.classList.add("matched");
-                    //set firstCard to false since next card would be first now
-                    firstCard = false;
-                    //winCount increment as user found a correct match
-                    winCount += 1;
-                    //ckeck if winCard == half of cardValues
-                    if (winCount == Math.floor(cardValues.length / 2)) {
-                        result.innerHTML = `<h2>You Won</h2>
-                        <h4>Moves: ${movesCount}</h4>`;
-                        stopGame();
-                    }
                 } else {
-
+                    //increment moves since user selected second card
+                    movesCounter();
+                    //secondCard and value
+                    secondCard = card;
+                    let secondCardValue = card.getAttribute("data-card-value");
+                    if (firstCardValue == secondCardValue) {
+                        //if both cards match add matched class so these cards would beignored next time
+                        firstCard.classList.add("matched");
+                        secondCard.classList.add("matched");
+                        //set firstCard to false since next card would be first now
+                        firstCard = false;
+                        //winCount increment as user found a correct match
+                        winCount += 1;
+                        //check if winCount ==half of cardValues
+                        if (winCount == Math.floor(cardValues.length / 2)) {
+                            result.innerHTML = `<h2>You Won</h2>
+              <h4>Moves: ${movesCount}</h4>`;
+                            stopGame();
+                        }
+                    } else {
+                        //if the cards dont match
+                        //flip the cards back to normal
+                        let [tempFirst, tempSecond] = [firstCard, secondCard];
+                        firstCard = false;
+                        secondCard = false;
+                        let delay = setTimeout(() => {
+                            tempFirst.classList.remove("flipped");
+                            tempSecond.classList.remove("flipped");
+                        }, 900);
+                    }
                 }
             }
         });
-    })
-}
+    });
+};
 
 //Initialize value and func call
 const initializer = () => {
