ad2cdd626d278aa81c0278fcc96424ba50492377
Bhoomika <bhoomikahm18@gmail.com>
2023-10-14 11:51:15 +0530

    Initial Time, moves and win count

--- script.js
+++ script.js
@@ -24,4 +24,25 @@ const items = [
     { name: "sloth", image: "sloth.png" },
     { name: "cockatoo", image: "cockatoo.png" },
     { name: "toucan", image: "toucan.png" },
-  ];
+];
+
+//Initial Time
+let secomds = 0,
+    minutes = 0;
+//Initial moves and win count
+let movesCount = 0,
+    winCount = 0;
+
+//For timer
+const timeGenerator = () => {
+    seconds += 1;
+    //minutes logic
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    }
+    //format time before displaying
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
